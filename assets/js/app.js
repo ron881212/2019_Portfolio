ffdc014916e50231f86e85ce92a1138d30d3f353
Ronald Glover <ron881212@gmail.com>
2019-01-18 22:38:40 -0500

    finished layout of all links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,30 @@
 $(document).on("click", "#portfolio", e=> {
-    $("#aboutContent").hide();
-    $("#portfolioContent").show();
     $("#portfolioContent").removeClass("hidden");
+    $("#aboutContent").hide();
+    $("#contactContent").hide();
+    $("#portfolioContent").show();
     $("h2").text("Portfolio");
 });
 
 $(document).on("click", "#about", e=> {
-    $("#aboutContent").show();
     $("#portfolioContent").hide();
+    $("#contactContent").hide();
+    $("#aboutContent").show();
     $("h2").text("About Me");
 });
+
+$(document).on("click", "#contact", e=> {
+    $("#contactContent").removeClass("hidden");
+    $("#aboutContent").hide();
+    $("#portfolioContent").hide();
+    $("#contactContent").show();
+    $("h2").text("Contact Me");
+});
+
+// $(document).mouseover(".contactInfo", e=> {
+//     $("#contactInfo").text("testing");
+//     console.log($(this).attr("data-info"));
+    
+// })
 
 
