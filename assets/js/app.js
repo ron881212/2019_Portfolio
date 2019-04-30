58be2adce3d3ccc0a5e95e21bdf66dde81335595
Ronald Glover <ron881212@gmail.com>
2019-04-30 05:28:22 -0400

    added winc stories

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ $(document).on("click", "#portfolio", e=> {
     $("#portfolioContent").removeClass("hidden");
     $("#aboutContent").hide();
     $("#contactContent").hide();
+    $(".page-footer").show();
     $("#portfolioContent").show();
     $("h2").text("Portfolio");
 });
@@ -9,6 +10,7 @@ $(document).on("click", "#portfolio", e=> {
 $(document).on("click", "#about", e=> {
     $("#portfolioContent").hide();
     $("#contactContent").hide();
+    $(".page-footer").show();
     $("#aboutContent").show();
     $("h2").text("About Me");
 });
@@ -17,6 +19,7 @@ $(document).on("click", "#contact", e=> {
     $("#contactContent").removeClass("hidden");
     $("#aboutContent").hide();
     $("#portfolioContent").hide();
+    $(".page-footer").hide();
     $("#contactContent").show();
     $("h2").text("Contact Me");
 });
